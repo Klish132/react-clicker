06e2fe0612949856b872d78bf97987c54ef05fff
Klish132 <shishkin@hrvr.academy>
2024-09-06 14:29:09 +0500

    Hide log out button when not logged in + blur

--- src/shared/ui/Navbar/Navbar.tsx
+++ src/shared/ui/Navbar/Navbar.tsx
@@ -6,20 +6,24 @@ import {Text} from "../Text/Text";
 import {useLocalStorage} from "../../lib/useLocalStorage";
 
 export const Navbar = () => {
-    const {logout} = useContext(AuthContext) || {};
+    const {isLoggedIn, logout} = useContext(AuthContext) || {};
     const [username, setUsername] = useLocalStorage<string | undefined>("username", undefined)
 
     return (
         <div className={styles.navbar}>
             <Text sizePx={16} color={"#A98467"}>{username}</Text>
             <div className={styles.navbarList}>
-                <Button isLarge={false} onClick={() => {
-                    logout!()
-                    setUsername(undefined)
-                }}
-                >
-                    Log out
-                </Button>
+                {isLoggedIn
+                    ?
+                    <Button isLarge={false} onClick={() => {
+                        logout!()
+                        setUsername(undefined)
+                    }}
+                    >
+                        Log out
+                    </Button>
+                    : <div/>
+                }
             </div>
         </div>
     );
